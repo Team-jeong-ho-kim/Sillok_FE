ff82415ca37c00222b57b4d72f7923bc9f996541
do-hyun123 <shickcoong777@gmail.com>
2024-12-21 21:57:29 +0900

    fix : router fix

--- packages/user/src/router.tsx
+++ packages/user/src/router.tsx
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
     element: <Header />,
     children: [
       {
-        path: '/feed',
+        path: '/',
         element: <Home />
       }
     ],
