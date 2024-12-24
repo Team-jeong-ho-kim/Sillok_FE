71b70c77bde6bb76e721238eedf14f2119f2aeb9
do-hyun123 <shickcoong777@gmail.com>
2024-12-25 00:25:14 +0900

    fix :url 수정

--- packages/user/src/apis/feed/index.ts
+++ packages/user/src/apis/feed/index.ts
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { instance } from "../axios";
 import { FeedResponse } from "./type";
+import axios from "axios";
 
-const router = "/feed";
+const router = "feed";
 
 export const useGetFeed = () => {
   return useQuery({
     queryKey: ["getFeedInfo"],
     queryFn: async () => {
-      const { data } = await instance.get<FeedResponse[]>(`${router}`);
+      const { data } = await axios.get<FeedResponse[]>(`https://sillok-stag-server.xquare.app/${router}`);
       return data;
     }
   })
